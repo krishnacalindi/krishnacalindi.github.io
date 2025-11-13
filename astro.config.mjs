09abcc46ed5c64ba1e87b3e416552c9d1b0e4229
Krishna Calindi <kxc@tamu.edu>
2025-11-13 03:28:37 -0600

    clean link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://krishnacalindi.github.io/",
-  base: "/portfolio",
   vite: {
     plugins: [tailwindcss()],
   },
