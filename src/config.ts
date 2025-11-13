b8ab46a1ec249e501408f858596eeea19d99af75
Krishna Calindi <kxc@tamu.edu>
2025-11-13 03:16:00 -0600

    setup

---
+++ src/config.ts
@@ -0,0 +1,79 @@
+export const siteConfig = {
+  firstName: "Krishna",
+  lastName: "Calindi",
+  name: "Krishna Calindi",
+  title: "Machine Learning Engineer",
+  description: "",
+  accentColor: "#2a8562",
+  social: {
+    email: "kxc@tamu.edu",
+    linkedin: "https://www.linkedin.com/in/krishna-calindi/",
+    github: "https://github.com/krishnacalindi/",
+  },
+  aboutMe:
+    "MS in CS grad student at Texas A&M University, working under Dr. Timothy Logan. Currently building a model to predict severe hail, wind and tornadoes.",
+  skills: ["Python", "Imbalanced learning", "Constrained optimization"],
+  publications: [
+    {
+      citation: "T. Logan et al., ”Aggie XLMA: A Python based alternative to XLMA”, under preparation.",
+      link: ""
+    },
+    {
+      citation: "T. Logan et al., “Comparison study of the electrical nature of two smoke-enhanced sea breeze thunderstorm cases during TRACER,” Journal of Geophysical Research - Atmospheres, 2025, under review.",
+      link: "vv"
+    },
+  ],
+  projects: [
+    {
+      name: "Aggie XLMA",
+      description:
+        "A Python-based alternative to XLMA. A Windows application to visualize, analyze and interact with lightning data.",
+      link: "https://github.com/krishnacalindi/hlma",
+      skills: ["Python"],
+    }
+  ],
+  experience: [
+    {
+      company: "Texas A&M University",
+      title: "Student Technician",
+      dateRange: "Aug 2024 - Present",
+      bullets: [
+        "Spearheaded the development of an application to process lightning data to improve research workflows.",
+        "Supported two research publications by analyzing data and producing high-quality figures.",
+        "Built a lightning-based warning model resulting in an abstract submission to 2 conferences",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "???",
+      degree: "PhD in Computer Science",
+      dateRange: "2026- ????",
+      achievements: [
+        "🤞",
+        "🚀",
+        "⭐",
+      ],
+    },
+    {
+      school: "Texas A&M University",
+      degree: "Master of Science in Computer Science with Thesis",
+      dateRange: "2024 - 2026",
+      achievements: [
+        "Graduated with 4.0 GPA",
+        "Conducted research on severe storm prediction",
+        "Contriubted to a publication in Journal of Geophysical Research",
+      ],
+    },
+    {
+      school: "University of Texas at Dallas",
+      degree: "Bachelor of Science in Computer Science with Minor in Business Administration",
+      dateRange: "2020 - 2024",
+      achievements: [
+        "Graduated with Collegium 5 Honors and 3.62 GPA",
+        "Achieved Academic Excellence Scholarship all semesters",
+        "Dean's List 3 semesters",
+      ],
+    },
+  ],
+};
